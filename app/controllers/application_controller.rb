372e06d045c9e778299fa94b1b1b4bfdaf799b02
Caroline Cunningham <ccunningham1@unc.edu>
2024-05-10 13:18:45 -0400

    point in polygon method working with geojson POST request and point hardcoded in controller logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
+    require_relative '../interactors/inside_polygon'
     def inside
-        # my_str = inside_polygon.testing_func()
-        data = { message: "success" }
+        #my_str = InsidePolygon.testing_func
+        # hash = JSON.parse request.body.read
+        # my_str = hash["note"]
+        #my_str = request.raw_post
+        my_str2 = InsidePolygon.point_in_poly([19.891395329260575, 0.2818513459221208], request.body.read)
+        data = { message: my_str2 }
 
         render json: data
     end
--- app/interactors/inside_polygon.rb
+++ app/interactors/inside_polygon.rb
@@ -1,47 +1,49 @@
 require 'json'
-def testing_func()
-    return "working!"
-end
-#does a horizontal scan to determine if the point is inside, need to look into edge cases more
-def point_in_poly(point, geojson)
-    polygon = json_to_polygon(geojson)
-    num_vertices = polygon.length
-    x, y = point[0], point[1]
-    inside = false
-   
-    #check if point is entirely out of range and go no furthur....
-    if x < polygon.map(&:first).min || x > polygon.map(&:first).max || y < polygon.map(&:last).min || y > polygon.map(&:last).max
-        puts "completely out of bounds"
-        return false
+module InsidePolygon
+    def self.testing_func
+        return "hi"
     end
-
-    p1 = polygon[0]
+    #does a horizontal scan to determine if the point is inside, need to look into edge cases more
+    def self.point_in_poly(point, geojson)
+        polygon = self.json_to_polygon(geojson)
+        num_vertices = polygon.length
+        x, y = point[0], point[1]
+        inside = false
     
-    (1..num_vertices).each do |i|
-        p2 = polygon[i % num_vertices]
+        #check if point is entirely out of range and go no furthur....
+        if x < polygon.map(&:first).min || x > polygon.map(&:first).max || y < polygon.map(&:last).min || y > polygon.map(&:last).max
+            puts "completely out of bounds"
+            return false
+        end
+
+        p1 = polygon[0]
         
-        if y > [p1[1], p2[1]].min
-            if y <= [p1[1], p2[1]].max
-                if x <= [p1[0], p2[0]].max
-                    if (p2[1] - p1[1]) != 0
-                        x_intersection = (y - p1[1]) * (p2[0] - p1[0]) / (p2[1] - p1[1]) + p1[0]
-                        if p1[0] == p2[0] || x <= x_intersection
+        (1..num_vertices).each do |i|
+            p2 = polygon[i % num_vertices]
+            
+            if y > [p1[1], p2[1]].min
+                if y <= [p1[1], p2[1]].max
+                    if x <= [p1[0], p2[0]].max
+                        if (p2[1] - p1[1]) != 0
+                            x_intersection = (y - p1[1]) * (p2[0] - p1[0]) / (p2[1] - p1[1]) + p1[0]
+                            if p1[0] == p2[0] || x <= x_intersection
+                                inside = !inside
+                            end
+                        else
                             inside = !inside
                         end
-                    else
-                        inside = !inside
                     end
                 end
             end
-        end
-        
-        p1 = p2
-    end 
-    return inside
-end
-def json_to_polygon(geojson)
-    obj = JSON.parse(geojson)
-    poly_wrapped = obj['geometry']
-    poly_unwrapped = polyg['coordinates']
-    return poly_unwrapped
+            
+            p1 = p2
+        end 
+        return inside
+    end
+    def self.json_to_polygon(geojson)
+        obj = JSON.parse(geojson)
+        poly_wrapped = obj['geometry']
+        poly_unwrapped = poly_wrapped['coordinates']
+        return poly_unwrapped[0]
+    end
 end
