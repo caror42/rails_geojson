f7307c97429d1b55bc1dd6057765577e91a949d8
Caroline Cunningham <ccunningham1@unc.edu>
2024-05-13 15:30:05 -0400

    .create is a key word; modified method name of Boundary method accordingly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
     require_relative '../interactors/inside_polygon'
+    require_relative '../models/boundary'
     def inside
         #my_str = InsidePolygon.testing_func
         # hash = JSON.parse request.body.read
@@ -13,6 +14,15 @@ class ApplicationController < ActionController::API
     def boundary
         #impliment logic to check if zip already exists
         #impliment logic to make a new entry into the database
-        Boundary.create
+        my_str2 = Boundary.make
+        data = {message: my_str2}
+        render json: data
+        # Boundary.create(
+        #     minx: 0.3,
+        #     maxx: 1.3,
+        #     miny: 0.31,
+        #     maxy: 1.31,
+        #     coordinates: "I am on Railsyeah!"
+        # )
     end
 end
--- app/models/boundary.rb
+++ app/models/boundary.rb
@@ -1,11 +1,15 @@
 class Boundary < ApplicationRecord
-    def create
-        # boundary = Boundary.new(minx: 0.3, maxx: 1.3, miny: 0.31, maxy: 1.31,  coordinates: "I am on Railsyeah!")
-        # boundary.save
-        promise = Concurrent::Promise.execute { boundary = Boundary.new(minx: 0.3, maxx: 1.3, miny: 0.31, maxy: 1.31,  coordinates: "I am on Railsyeah!")}
-
-        promise.then do |result|
-            boundary.save
-        end
+    def make
+        #boundary = Boundary.new(:minx => 0.3, :maxx => 1.3, :miny => 0.31, :maxy => 1.31,  :coordinates => "I am on Railsyeah!")
+        # attributes = {minx: 0.3, maxx: 1.3, miny: 0.31, maxy: 1.31,  coordinates: "I am on Railsyeah!"}
+        # boundary = Boundary.new(attributes)
+        Boundary.create(
+            minx: 0.3,
+            maxx: 1.3,
+            miny: 0.31,
+            maxy: 1.31,
+            coordinates: "I am on Railsyeah!"
+        )
+        return "hey"
     end
 end
