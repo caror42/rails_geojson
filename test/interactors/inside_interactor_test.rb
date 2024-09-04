1f523b5662ebecbd0218c6b4db4109e8f6b97b65
Caroline Cunningham <carolinecunningham@Carolines-MBP.localdomain>
2024-09-04 14:48:25 -0700

    Test: Inside Polygon interactor test. Fix: correct Boundary fixture 1 coordinates

--- test/interactors/inside_interactor_test.rb
+++ test/interactors/inside_interactor_test.rb
@@ -1,9 +1,37 @@
 require "test_helper"
 
 class InsideInteractorTest < ActionDispatch::IntegrationTest
+  test "inside rectangle" do
+    assert_equal(true, InsidePolygon.point_in_poly(@params))
+  end
+  test "outside rectangle" do
+    assert_equal(false, InsidePolygon.point_in_poly(@outside_params))
+  end
+  test "missing point param" do
+    assert_raise RuntimeError do
+        InsidePolygon.point_in_poly(@id_param_only)
+    end
+  end
+  test "missing id param" do
+    assert_raise RuntimeError do
+        InsidePolygon.point_in_poly(@point_param_only)
+    end
+  end
   setup do
     @boundary = boundaries(:one)
+    @params = {
+        "point": [1,1.5],
+        "id": @boundary.id
+    }
+    @outside_params = {
+        "point": [5,8],
+        "id": @boundary.id
+    }
+    @id_param_only = {
+        "id": @boundary.id
+    }
+    @point_param_only = {
+        "point": [1,1.5]
+    }
   end
-
-
 end
