e20cf7556ebc28b9b5bfdda1ddedd6f4cb4ae60c
Caroline Cunningham <carorcunningham@gmail.com>
2024-10-07 16:41:33 -0400

    Feat: Index boundaries_users by user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_07_184351) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_07_204007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_07_184351) do
   create_table "boundaries_users", id: false, force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "boundary_id", null: false
+    t.index ["user_id"], name: "index_boundaries_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
