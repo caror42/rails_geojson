9a12415a5b7ae853bf86d32b289789e51a58aa87
Caroline Cunningham <carorcunningham@gmail.com>
2024-10-16 15:24:07 -0400

    Style: remove TODO

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
-  #TODO: don't let any user create other users
   # GET /users
   def index
     @users = User.all
